e3fc639cdc0e79a1167f66ed66e18baa7695be63
tygat <tygat@Tylers_Laptop.ad.wsc.edu>
2023-03-01 09:50:04 -0600

    Started intersection

--- Intersection.java
+++ Intersection.java
@@ -1,8 +1,14 @@
 
 public class Intersection {
 
-	public Intersection() {
-		// TODO Auto-generated constructor stub
+	private Street street1;
+	private Street street2;
+	
+	public Intersection(String street1Name, String street2Name) {
+		this.street1 = new Street(street1Name);
+		this.street2 = new Street(street2Name);
 	}
 
+	public set
+	
 }
